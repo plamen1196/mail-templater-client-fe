8c671537a7b44d1952396a561e1711ff312feabb
tachojelev <tachojelev@gmail.com>
2022-05-23 15:16:48 +0300

    Add initial styling and dependencies

---
+++ src/api/email-templater-api.ts
@@ -0,0 +1,12 @@
+import { environment } from './../environments/environment';
+
+const API_URL = environment.apiUrl;
+
+/**
+ * Class that contains the API endpoints for the Email Templater Backend Service.
+ */
+ export class EmailTemplaterApi {
+
+    /* Confirmation */
+    public static readonly CONFIRM_EMAIL: string = `${API_URL}/`;
+}
